523078a9f4d6b3736f85b9f405d3c04d79d70125
varavin1 <varavin1@LIN73001567.corp.capgemini.com>
2022-01-19 01:20:38 +0530

    Application TC

--- AdminCompositeConsumer/src/test/java/com/bugtracking/admin/AdminApplicationTests.java
+++ AdminCompositeConsumer/src/test/java/com/bugtracking/admin/AdminApplicationTests.java
@@ -1,13 +1,15 @@
 package com.bugtracking.admin;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class AdminApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
-
 }
